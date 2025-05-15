829be911a7eebf84291bebfb4a37d74f6ce4c9b3
Flameis <Rocketgunslinger@gmail.com>
2025-05-14 22:41:52 -0700

    Test 1

--- project_4/run.sh
+++ project_4/run.sh
@@ -6,7 +6,7 @@ echo "bSIMD,ARRAYSIZE,SPEEDUP" > results.csv
 for n in 1024 2048 4096 8192 16384 32768 65536 131072 262144 524288 1048576 2097152 4194304 8388608
 do
     # Compile the program with the current array size
-    g++ proj04 proj04.cpp -lm -fopenmp -DARRAYSIZE=$n
+    g++ -o proj04 proj04.cpp -lm -fopenmp -DARRAYSIZE=$n
     
     # Check if compilation was successful
     if [ $? -eq 0 ]; then
