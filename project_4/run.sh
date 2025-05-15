14116770b194f278082b2f3b8281cdc8ddba012a
Flameis <Rocketgunslinger@gmail.com>
2025-05-14 21:51:17 -0700

    Test 1

--- project_4/run.sh
+++ project_4/run.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 # This script compiles the C++ program and runs it with different array sizes from 1K to 8M
+echo "ARRAYSIZE,SUMN,SUMS,MEGAMULTS,MEGAMULTADDS,SPEEDUP" > results.csv
+
 for n in 1024 2048 4096 8192 16384 32768 65536 131072 262144 524288 1048576 2097152 4194304 8388608
 do
-    # echo "ARRAYSIZE,SUMN,SUMS,MEGAMULTS,MEGAMULTADDS,SPEEDUP" > results.csv
-
-    t=$n * $n
-    g++ -o proj04 proj04.cpp -fopenmp -msse4.1 -O3
+    # Compile the program with the current array size
+    g++ -o proj04 proj04.cpp -lm -fopenmp -msse4.1 -O3 -DARRAYSIZE=$n
     
     # Check if compilation was successful
     if [ $? -eq 0 ]; then
-        # Run the program
+        # Run the program and append output to results.csv
         ./proj04 >> results.csv
     else
-        echo "Compilation failed"
+        echo "Compilation failed for array size $n"
     fi
 done
 
