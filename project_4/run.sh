c884e85d226693065b13230c5e4073bf1be66a4e
Flameis <Rocketgunslinger@gmail.com>
2025-05-14 21:18:13 -0700

    Test 1

--- project_4/run.sh
+++ project_4/run.sh
@@ -4,9 +4,9 @@
 for n in 1024 2048 4096 8192 16384 32768 65536 131072 262144 524288 1048576 2097152 4194304 8388608
 do
     t = $n * $n
-    g++ all04.cpp -ARRAYSIZE=t -o proj04 -lm -fopenmp
+    g++ proj04.cpp -ARRAYSIZE=t -o proj04 -lm -fopenmp
     
-    ./all04 >> results.csv
+    ./proj04 >> results.csv
 done
 
 echo "Results have been saved to results.csv"
