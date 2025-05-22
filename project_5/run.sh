5ed617f7c12b452245b97eab9c2bf57ec85bd9af
Flameis <Rocketgunslinger@gmail.com>
2025-05-21 21:48:41 -0700

    Project 5 Init

---
+++ project_5/run.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+for t in 1024 4096 16384 65536 262144 1048576 2097152
+do
+    for b in 8 32 64 128 256
+    do
+            /usr/local/apps/cuda/cuda-10.1/bin/nvcc -DNUMTRIALS=$t -DBLOCKSIZE=$b -o proj05  proj05.cu
+            ./proj05
+    done
+done
+
+On the DGX system, here is a working sbatch script:
+
+
+#!/bin/bash
+#SBATCH  -J  MonteCarlo
+#SBATCH  -A  cs475-575
+#SBATCH  -p  classgputest
+#SBATCH  --gres=gpu:1
+#SBATCH  -o  montecarlo.out
+#SBATCH  -e  montecarlo.err
+#SBATCH  --mail-type=BEGIN,END,FAIL
+#SBATCH  --mail-user=mjb@oregonstate.edu
+for t in 2048 8192 131072 2097152
+do
+    for b in 8 16 32 64 128
+    do
+            /usr/local/apps/cuda/11.7/bin/nvcc -DNUMTRIALS=$t -DBLOCKSIZE=$b -o proj05  proj05.cu
+            ./proj05
+    done
+done
+
