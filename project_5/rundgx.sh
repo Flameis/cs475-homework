282fb1275d28efcfc8ae815e094634050e4acf57
Flameis <Rocketgunslinger@gmail.com>
2025-05-22 13:54:53 -0700

    testing

--- project_5/rundgx.sh
+++ project_5/rundgx.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH  -J  MonteCarlo
 #SBATCH  -A  cs475-575
-#SBATCH  -p  classgputest
+#SBATCH  -p  classgpufinal
 #SBATCH  --gres=gpu:1
 #SBATCH  -o  montecarlo.out
 #SBATCH  -e  montecarlo.err
@@ -12,6 +12,6 @@ do
     for b in 8 32 64 128 256
     do
             /usr/local/apps/cuda/11.7/bin/nvcc -DNUMTRIALS=$t -DBLOCKSIZE=$b -o proj05  proj05.cu
-            ./proj05 >> results.csv
+            ./proj05
     done
 done
