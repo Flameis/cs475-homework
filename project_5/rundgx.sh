f9701643a64817e73c326e324ad84fbc28e6fbbb
Flameis <Rocketgunslinger@gmail.com>
2025-05-22 14:00:53 -0700

    testing

--- project_5/rundgx.sh
+++ project_5/rundgx.sh
@@ -5,8 +5,6 @@
 #SBATCH  --gres=gpu:1
 #SBATCH  -o  montecarlo.out
 #SBATCH  -e  montecarlo.err
-#SBATCH  --mail-type=BEGIN,END,FAIL
-#SBATCH  --mail-user=scovell@oregonstate.edu
 for t in 1024 4096 16384 65536 262144 1048576 2097152
 do
     for b in 8 32 64 128 256
