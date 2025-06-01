bb43a759463c2ab24ea22cd55aae767b3e11e22a
Flameis <Rocketgunslinger@gmail.com>
2025-06-01 00:01:21 -0700

    project-6

--- project_6/runrab.sh
+++ project_6/runrab.sh
@@ -3,7 +3,7 @@ for t in 1024 4096 16384 65536 262144 1048576 2097152
 do
     for b in 8 32 64 128 256
     do
-        g++ -o proj06 proj06.cpp -lm -fopenmp -DATASIZE=$t -LOCALSIZE=$b
+        g++ -o proj06 proj06.cpp -lm -fopenmp -lOpenCL -DATASIZE=$t -LOCALSIZE=$b
         ./proj06
     done
 done
