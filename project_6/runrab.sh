66eb5d89d966d0233ae6a764ea957bca655d4717
Flameis <Rocketgunslinger@gmail.com>
2025-06-01 19:24:26 -0700

    proj6csv

--- project_6/runrab.sh
+++ project_6/runrab.sh
@@ -4,6 +4,6 @@ do
         for b in 8 32 64 128 256
         do
                 g++ -DDATASIZE=$s -DLOCALSIZE=$b -o proj06 proj06.cpp /usr/local/apps/cuda/10.1/lib64/libOpenCL.so.1  -lm -fopenmp
-                ./proj06
+                ./proj06 >> results.csv
         done
 done
