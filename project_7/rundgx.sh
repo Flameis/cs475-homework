202214ec9b4c141fccd8006a9129811384e61b85
Flameis <Rocketgunslinger@gmail.com>
2025-06-08 00:42:32 -0700

    Proj7

--- project_7/rundgx.sh
+++ project_7/rundgx.sh
@@ -8,6 +8,9 @@
 #SBATCH -e mpiproject.err
 #SBATCH  --mail-type=BEGIN,END,FAIL
 #SBATCH  --mail-user=scovell@oregonstate.edu
+unset PERL5LIB
+export PERL5LIB=""
+
 module load openmpi
 mpic++ proj07.cpp -o proj07 -lm
 for b in 4 8 12 16 32
