bdc2860e9740ec98d40a71c71e872da3390a29b0
Flameis <Rocketgunslinger@gmail.com>
2025-06-08 00:27:35 -0700

    Proj7

--- project_7/rundgx.sh
+++ project_7/rundgx.sh
@@ -10,7 +10,7 @@
 #SBATCH  --mail-user=scovell@oregonstate.edu
 module load openmpi
 mpic++ -o proj07 proj07.cpp -lm -fopenmp
-for b in 4 8 12 16 32 48 64
+for b in 4 8 12 16 32
 do
         mpiexec -mca btl self,tcp -np $b ./proj07
 done
