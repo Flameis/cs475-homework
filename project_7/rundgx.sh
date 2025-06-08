56c9e8de3d83f9f768a44ed60bb732880a4532b9
Flameis <Rocketgunslinger@gmail.com>
2025-06-08 12:01:30 -0700

    proj7test

--- project_7/rundgx.sh
+++ project_7/rundgx.sh
@@ -2,16 +2,24 @@
 #SBATCH -J Fourier
 #SBATCH -A cs475-575
 #SBATCH -p classmpitest
-#SBATCH -N 8 # number of nodes
-#SBATCH -n 8 # number of tasks
+#SBATCH -N 8
+#SBATCH -n 8
 #SBATCH -o mpiproject.out
 #SBATCH -e mpiproject.err
-#SBATCH  --mail-type=END,FAIL
-#SBATCH  --mail-user=scovell@oregonstate.edu
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=scovell@oregonstate.edu
 
 module load openmpi
-for b in 1 2 4 6 8
+
+# Unset any user Perl environment to avoid breaking mpiexec
+unset PERL5LIB
+unset PERL_LOCAL_LIB_ROOT
+unset PERL_MB_OPT
+unset PERL_MM_OPT
+
+mpic++ proj07.cpp -o proj07 -lm
+
+for b in 4 8 12 16 32
 do
-        mpic++ proj07.cpp -o proj07 -lm
-        mpiexec -mca btl self,tcp -np $b ./proj07
+    mpiexec -mca btl self,tcp -np $b ./proj07
 done
